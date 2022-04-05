2eca18ee17a04d84f7429559ad68104514bb8ab2
DovgialloHanna <dovgiallohanna@gmail.com>
2022-04-05 20:08:55 +0300

    Change App.tsx(change syntax for "react-router-dom" v.5), index.tsx, README.md

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Header} from "./Components/Header/Header";
 import {NavBar} from "./Components/NavBar/NavBar";
 import {Profile} from "./Components/Profile/Profile";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
 import {Music} from "./Components/Music/Music";
 import {News} from "./Components/News/News";
@@ -17,21 +17,21 @@ const App = () => {
                 <Header/>
                 <NavBar/>
                 <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path='/dialogs' element={<Dialogs/>}/>
-                    </Routes>
-                    <Routes>
-                        <Route path='/profile' element={<Profile/>}/>
-                    </Routes>
-                    <Routes>
-                        <Route path='/news' element={<News/>}/>
-                    </Routes>
-                    <Routes>
-                        <Route path='/music' element={<Music/>}/>
-                    </Routes>
-                    <Routes>
-                        <Route path='/setting' element={<Setting/>}/>
-                    </Routes>
+
+                        <Route path='/dialogs' component={Dialogs}/>
+
+
+                        <Route path='/profile' component={Profile}/>
+
+
+                        <Route path='/news' component={News}/>
+
+
+                        <Route path='/music' component={Music}/>
+
+
+                        <Route path='/setting' component={Setting}/>
+
 
                 </div>
 
